42851423259c517a4fcdadc4b775a20e2c6033c6
Libertas <horizonvei@gmail.com>
2012-04-14 18:11:42 +0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+test2bf-c:test2bf.c
+	cc test2bf.c -o test2bf-c -O2 --std=c99
---
+++ test2bf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUF 0xffff
+
+int main()
+{
+        char *test;
+        int word=0;
+        test=calloc(BUF,sizeof(int));
+        scanf("%s",test);
+        for(int i=0;i<strlen(test);i++)
+        {
+                for(word=(int)test[i];word>0;word--) printf("+");
+                printf(".[-]\n");
+        }
+        free(test);
+        return 0;
+}
